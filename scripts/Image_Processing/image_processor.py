b16124785e2106ae93cc208e08cf002cdd1fe0c9
Adam Austin <adam.marcus.austin@gmail.com>
2019-12-04 14:53:51 -0700

    Added a little bit of structure for CV

--- scripts/Image_Processing/image_processor.py
+++ scripts/Image_Processing/image_processor.py
@@ -4,3 +4,17 @@ class ImageProcessor:
 
     def test_method(self):
         return True
+
+    def image_evaluate_features(self):
+        # Call mask_land_mark and post the image to the topic for consumption by the swarm controller
+        
+        # Call mask_path and post the image to the topic for consumption by the swarm controller
+
+        return True
+
+    # This method returns an image where a mask is applied only to pixels in the image that are determined to constitute a landmark
+    def mask_land_mark(self):
+    
+    # This method returns an image where a mask is applied only to pixels in the image that are determined to constitute the path
+    def mask_path(self):
+        
