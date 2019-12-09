9379818eba22aafeeee09512fab61f78405fc914
RS-Coop <simpson.r.cooper@gmail.com>
2019-12-08 19:40:13 -0700

    Changing some stuff in Image Processing

--- scripts/Image_Processing/image_processor.py
+++ scripts/Image_Processing/image_processor.py
@@ -8,20 +8,28 @@ class ImageProcessor:
         #Publishers
 
         #Subscribers
-        self.image_sub = rospy.Subscriber('/swarm/drone_image', TaggedImage, self.placeholder())
+        self.image_sub = rospy.Subscriber('/swarm/drone_image', TaggedImage, self.image_callback())
 
         sleep(1.0)
 
-    def test_method(self):
-        return True
+################################################################################
+#Main methods and publisher methods
+    #Loop to be run in main script
+    #DONE: I think this works
+    def run_node(self):
+        while not rospy.is_shutdown():
+            continue
 
-    # Takes in a
+    #Takes in a
+    #TODO: What does this do
     def image_evaluate_features_from_file(self, img_loc):
         img = cv.imread(img_loc)
         self.image_evaluate_features_from_matrix(img)
 
         return True
 
+    #What does this do?
+    #TODO:
     def image_evaluate_features_from_matrix(self, img):
         # Call mask_land_mark and post the image to the topic for consumption by the swarm controller
         self.mask_land_mark(img)
@@ -29,13 +37,30 @@ class ImageProcessor:
 
         return True
 
-    # This method returns an image where a mask is applied only to pixels in the image that are determined to constitute a landmark
+    #Returns an image where a mask is applied only to pixels in the image
+    #that are determined to constitute a landmark
+    #DONE
     def mask_land_mark(self, img):
         print(img)
         return True
 
-    # This method returns an image where a mask is applied only to pixels in the image that are determined to constitute the path
+    #Returns an image where a mask is applied only to pixels in the image
+    #that are determined to constitute the path
     def mask_lines(self, img):
         img = cv.imread(img,0)
         edges = cv.Canny(img,700,750)
         return edges
+
+################################################################################
+#Callbacks
+    #Proccesses image from drone
+    #TODO: What will this call or do?
+    def image_callback(self,data):
+        pass
+
+################################################################################
+#Tests
+    #Not sure what this does
+    #TODO:
+    def test_method(self):
+        return True
