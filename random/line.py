43c0df392fceb08c05ff719e636039226aab9d89
RS-Coop <simpson.r.cooper@gmail.com>
2019-12-11 11:09:45 -0700

    Figured out one way to do the angle, will maybe try regression as well

--- random/line.py
+++ random/line.py
@@ -43,15 +43,36 @@ cv.destroyAllWindows()
 dims = image.shape
 blank = np.zeros((dims[0],dims[1],3),np.uint8)
 lines = cv.HoughLinesP(edges,1,np.pi/180,10,10)
+angle = 0
+num_lines = 0
+sum_adj = 0
+sum_opp = 0
 
 for line in lines:
     for x1,y1,x2,y2 in line:
         len = np.sqrt((x1-x2)**2+(y1-y2)**2)
         if len > 5:
             cv.line(blank,(x1,y1),(x2,y2),(0,255,0),2)
+            # print(line)
+            adj = np.abs(y1-y2)
+            opp = np.abs(x1-x2)
+            if adj > 0:
+                angle += np.arctan(opp/adj)
+                print('Opposite: %d, Adjacent: %d', opp, adj)
+                num_lines += 1
+                sum_opp += opp
+                sum_adj += adj
+
+print('Number of lines: %d', num_lines)
+print(sum_opp/num_lines, sum_adj/num_lines)
+print("Angle w/t vertical: %d", np.degrees(angle/num_lines))
+num = (float(sum_opp/num_lines))/float((sum_adj/num_lines))
+print('num: ',num)
+angle = np.arctan(num)
+print("Angle w/t vertical: %d", np.degrees(angle), angle)
 
 cv.imshow('Image',blank)
-cv.waitKey(5)
+cv.waitKey(0)
 cv.destroyAllWindows()
 
 # lines = cv.HoughLines(edges,1,np.pi/180,10)
