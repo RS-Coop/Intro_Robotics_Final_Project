d59d6efd4268057e02be393c31d0d5a3fb84959f
RS-Coop <simpson.r.cooper@gmail.com>
2019-12-11 12:20:55 -0700

    Color filter and line_angle are ready to test

--- scripts/Image_Processing/image_processor.py
+++ scripts/Image_Processing/image_processor.py
@@ -5,7 +5,7 @@ import pyzbar.pyzbar as pyz
 #This class deals with processing images from the Bebop drones
 class ImageProcessor:
     #List of line color ranges
-    self.line_colors = [[5,100,150], [15,255,255]] #Oragne right now
+    self.line_colors = {'orange', [[5,100,150], [15,255,255]]} #Oragne right now
 
     def __init__(self):
         #Initialize pubs and subs
@@ -26,20 +26,45 @@ class ImageProcessor:
 
     #Takes an image and color filters for all
     #potential line colors to get line blobs
-    #TODO: Do this
-    def line_color_filter(self, image):
+    #DONE: Creates mask for specfied color or all colors if non-specific
+    def line_color_filter(self, image, color=None):
         hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
         #Right now this is just orange, I think it will need to be a loop
-        mask = cv.inRange(hsv, self.line_colors[0], self.line_colors[1])
+        if color == None:
+            mask = np.zeros((image.shape[0], image.shape[1],3),np.uint8)
+            for color in self.line_colors:
+                color_mask = cv.inRange(hsv, color[0], color[1])
+                mask = cv.bitwise_and(mask, color_mask)
+        else:
+            mask = cv.inRange(hsv, self.line_colors[color][0], self.line_colors[color][1])
+
+        return mask
 
     #Takes an image that is the isolated line blob
     #and returns the angle to vertical
-    #TODO: Figure out how to do this
+    #DONE: Calculates avg x and y components and then computes angle
+    #TODO: Test and tweak parameters
     def line_angle(self, line_mask):
         edges = cv.Canny(line_mask,50,150,apertureSize = 3)
         lines = cv.HoughLinesP(edges,1,np.pi/180,10)
 
         #Now to calculate angle from lines
+        sum_opp=0, sum_adj=0, num_lines=0
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                len = np.sqrt((x1-x2)**2+(y1-y2)**2)
+                if len > 5:
+                    adj = np.abs(y1-y2)
+                    opp = np.abs(x1-x2)
+                    if adj > 0:
+                        num_lines += 1
+                        sum_opp += opp
+                        sum_adj += adj
+
+        num = (float(sum_opp/num_lines))/float((sum_adj/num_lines))
+        angle = np.arctan(num)
+
+        return np.degrees(angle)
 
     #Takes a QR code image and proccess it.
     #DONE: Returns string of QR code data
