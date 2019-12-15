6d20798ad92d8e97a1921cb60c58df1c42e10bb2
Adam Austin <adam.marcus.austin@gmail.com>
2019-12-15 11:52:54 -0700

    Test fixes and adding framework for additional tests

--- scripts/cv_test.py
+++ scripts/cv_test.py
@@ -39,46 +39,82 @@ class TestImageProcessing(unittest.TestCase):
     # Requires an image with a QR code in it
     def test_dont_identify_no_qr(self):
         img = cv.imread(self.parent_dir+"/test_images/not_has_qr_code.jpg")
-        self.assertEqual(self.cv_processor.process_image(img)[0][0], False)
+        output = self.cv_processor.process_image(img)
+        print(output)
+        self.assertEqual(output["QR"]["hasQR"], False)
 
     # Identify that a QR code is NOT in the image (QR code partially showing)
     # Requires an image with a QR code in it
     def test_dont_identify_partial_qr(self):
         img = cv.imread(self.parent_dir+"/test_images/has_partial_qr_upper_left.jpg")
-        self.assertEqual(self.cv_processor.process_image(img)[0][0], False)
+        output = self.cv_processor.process_image(img)
+        print(output)
+        self.assertEqual(output["QR"]["hasQR"], False)
     
     # Identify that a QR code is in the image (entire QR code showing)
     # Requires an image with a QR code in it
     def test_identify_qr(self):
         img = cv.imread(self.parent_dir+"/test_images/has_qr_code.jpg")
-        self.assertEqual(self.cv_processor.process_image(img)[0][0], True)
+        output = self.cv_processor.process_image(img)
+        print(output)
+        self.assertEqual(output["QR"]["hasQR"], True)
+        self.assertEqual(output["QR"]["centroid"], (0, 0))
 
     # Identify the location of a QR code in the image
     # Requires an image with a QR code in it
+    # Upper 3rd
+    def test_identify_upper_third_qr(self):
+        pass
+    
 
     # Identify a QR code is not centered in the image (only location)
     # Requires an image with a QR code in it not centered
+    def test_identify_right_third_qr(self):
+        pass
 
     # Identify a QR code is centered in the image (only location)
     # Requires an image with a QR code in it centered centered
+    def test_identify_centered_qr(self):
+        pass
 
     #### Edge Identification 
     # Identify a single edge coming out of a vertex
     # Requires an image of a centered QR code with one color tape coming out of it
+    def test_identify_one_edge(self):
+        pass
 
     # Identify the angle a single edge is coming out of a vertex
     # Requires an image of a centered QR code with one color tape coming out of it
+    # Test edge at 90 degrees
+    def test_identify_one_edge_135_degrees(self):
+        pass
+
+    def test_identify_one_edge_90_degrees(self):
+        pass
+
+    def test_identify_one_edge_45_degrees(self):
+        pass
+
+    def test_identify_one_edge_0_degrees(self):
+        pass
+
+    def test_identify_one_edge_neg_45_degrees(self):
+        pass
+
+    def test_identify_one_edge_neg_90_degrees(self):
+        pass
+
+    def test_identify_one_edge_neg_135_degrees(self):
+        pass
     
     # Identify 2 edges coming out of a vertex
     # Requires an image of a centered QR code with two colors of tape coming out of it
+    def test_identify_two_edges(self):
+        pass
 
     # Identify the angles of 2 edges coming out of a vertex
     # Requires an image of a centered QR code with two colors of tape coming out of it
 
-
-    # def test_detect_partial_qr_code(self):
-    #     img = cv.imread("has_qr_code_lower_third_with_orange_line.jpg")
-    #     self.assertEqual(self.cv_processor.detect_QR_code(img), None)
 
 class TestDroneController():
     def __init__():
