42e85ba54720db88c4bd09a271d2256317826d6f
RS-Coop <simpson.r.cooper@gmail.com>
2019-12-17 11:56:41 -0700

    Line bands ready to test

--- random/line.py
+++ random/line.py
@@ -1,88 +1,168 @@
 import cv2 as cv
 import numpy as np
 
-image = cv.imread('../test_images/sample_purple_line2.jpg')
-cv.imshow('Image',image)
-cv.waitKey(0)
-cv.destroyAllWindows()
+O_TOP = 'outer top'
+O_BOTTOM = 'outer bottom'
+O_LEFT = 'outer left'
+O_RIGHT = 'outer right'
 
-#First filter for color
-hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-lower = np.array([100,0,0])
-upper = np.array([115,255,255])
-mask = cv.inRange(hsv, lower, upper)
-cv.imshow('Mask', mask)
+#Inner
+I_TOP = 'inner top'
+I_BOTTOM = 'inner bottom'
+I_LEFT = 'inner left'
+I_RIGHT = 'inner right'
 
-cv.waitKey(0)
-cv.destroyAllWindows()
-result = cv.bitwise_and(image, image, mask=mask)
-cv.imshow('Blob', result)
-cv.waitKey(0)
-cv.destroyAllWindows()
+def get_band_averages(mask):
+    band_avgs = {}
+    height = mask.shape[0]
+    width = mask.shape[1]
+    print(height)
+    print(width)
 
-'''
-gray = cv.cvtColor(result, cv.COLOR_HSV2GRAY)
-cv.imshow('Gray',gray)
-cv.waitKey(0)
-cv.destroyAllWindows()
-'''
-_,_,gray = cv.split(result)
+    sum_y = 0
+    sum_x = 0
+    num_pixels = 0
 
+    for row in range(5):
+        for col in range(width):
+            if mask[row, col] == 255:
+                sum_x += col
+                sum_y += row
+                num_pixels += 1
+    print(num_pixels)
+    if num_pixels > 5:
+        band_avgs.update({O_TOP:(sum_x/num_pixels, sum_y/num_pixels)})
+    else:
+        band_avgs.update({O_TOP:(None,None)})
+###############################################################################
+    sum_y = 0
+    sum_x = 0
+    num_pixels = 0
 
-edges = cv.Canny(mask,50,150,apertureSize = 3)
-cv.imshow('Edges', edges)
-cv.waitKey(0)
-cv.destroyAllWindows()
+    for row in range(height-6, height):
+        for col in range(width):
+            if mask[row, col] == 255:
+                sum_x += col
+                sum_y += row
+                num_pixels += 1
 
-# lines = cv.HoughLinesP(edges,1,np.pi/180,10)
-# for x1,y1,x2,y2 in lines[0]:
-#     len = np.sqrt((x1-x2)**2+(y1-y2)**2)
-#     print(len)
-#     if len > 100:
-#         cv.line(image,(x1,y1),(x2,y2),(0,255,0),2)
-dims = image.shape
-blank = np.zeros((dims[0],dims[1],3),np.uint8)
-lines = cv.HoughLinesP(edges,1,np.pi/180,10,10)
-angle = 0
-num_lines = 0
-sum_adj = 0
-sum_opp = 0
+    if num_pixels > 5:
+        band_avgs.update({O_BOTTOM:(sum_x/num_pixels, sum_y/num_pixels)})
+    else:
+        band_avgs.update({O_BOTTOM:(None,None)})
+###############################################################################
+    sum_y = 0
+    sum_x = 0
+    num_pixels = 0
 
-print('Number of Lines: ', len(lines))
+    for row in range(height):
+        for col in range(5):
+            if mask[row, col] == 1:
+                sum_x += col
+                sum_y += row
+                num_pixels += 1
 
-for line in lines:
-    for x1,y1,x2,y2 in line:
-        len = np.sqrt((x1-x2)**2+(y1-y2)**2)
-        if len > 5:
-            cv.line(blank,(x1,y1),(x2,y2),(0,255,0),2)
-            # print(line)
-            adj = np.abs(y1-y2)
-            opp = np.abs(x1-x2)
-            if adj > 0:
-                angle += np.arctan(opp/adj)
-                print('Opposite: %d, Adjacent: %d', opp, adj)
-                num_lines += 1
-                sum_opp += opp
-                sum_adj += adj
+    if num_pixels > 5:
+        band_avgs.update({O_LEFT:(sum_x/num_pixels, sum_y/num_pixels)})
+    else:
+        band_avgs.update({O_LEFT:(None,None)})
+###############################################################################
+    sum_y = 0
+    sum_x = 0
+    num_pixels = 0
 
-print('Number of lines: %d', num_lines)
-print(sum_opp/num_lines, sum_adj/num_lines)
-print("Angle w/t vertical: %d", np.degrees(angle/num_lines))
-num = (float(sum_opp/num_lines))/float((sum_adj/num_lines))
-print('num: ',num)
-angle = np.arctan(num)
-print("Angle w/t vertical: %d", np.degrees(angle), angle)
+    for row in range(height):
+        for col in range(width-6, width):
+            if mask[row, col] == 1:
+                sum_x += col
+                sum_y += row
+                num_pixels += 1
 
-cv.imshow('Image',blank)
-cv.waitKey(0)
-cv.destroyAllWindows()
+    if num_pixels > 5:
+        band_avgs.update({O_RIGHT:(sum_x/num_pixels, sum_y/num_pixels)})
+    else:
+        band_avgs.update({O_RIGHT:(None,None)})
+###############################################################################
+    sum_y = 0
+    sum_x = 0
+    num_pixels = 0
 
-# lines = cv.HoughLines(edges,1,np.pi/180,10)
-# sum = 0
-# num = 0
-# for line in lines:
-#     for _,theta in line:
-#         sum += theta
-#         num += 1
-#
-# print(np.degrees(sum/num))
+    for row in range(5):
+        for col in range(width):
+            if mask[row, col] == 1:
+                sum_x += col
+                sum_y += row
+                num_pixels += 1
+
+    if num_pixels > 5:
+        band_avgs.update({I_TOP:(sum_x/num_pixels, sum_y/num_pixels)})
+    else:
+        band_avgs.update({I_TOP:(None,None)})
+###############################################################################
+    sum_y = 0
+    sum_x = 0
+    num_pixels = 0
+
+    for row in range(height-6, height):
+        for col in range(width):
+            if mask[row, col] == 1:
+                sum_x += col
+                sum_y += row
+                num_pixels += 1
+
+    if num_pixels > 5:
+        band_avgs.update({I_BOTTOM:(sum_x/num_pixels, sum_y/num_pixels)})
+    else:
+        band_avgs.update({I_BOTTOM:(None,None)})
+###############################################################################
+    sum_y = 0
+    sum_x = 0
+    num_pixels = 0
+
+    for row in range(height):
+        for col in range(5):
+            if mask[row, col] == 1:
+                sum_x += col
+                sum_y += row
+                num_pixels += 1
+
+    if num_pixels > 5:
+        band_avgs.update({I_LEFT:(sum_x/num_pixels, sum_y/num_pixels)})
+    else:
+        band_avgs.update({I_LEFT:(None,None)})
+###############################################################################
+    sum_y = 0
+    sum_x = 0
+    num_pixels = 0
+
+    for row in range(height):
+        for col in range(width-6, width):
+            if mask[row, col] == 1:
+                sum_x += col
+                sum_y += row
+                num_pixels += 1
+
+    if num_pixels > 5:
+        band_avgs.update({I_RIGHT:(sum_x/num_pixels, sum_y/num_pixels)})
+    else:
+        band_avgs.update({I_RIGHT:(None,None)})
+###############################################################################
+    return band_avgs
+
+if __name__=='__main__':
+    image = cv.imread('../test_images/sample_blue_line.jpg')
+    cv.imshow('Image',image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    #First filter for color
+    hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    lower = np.array([100,0,0])
+    upper = np.array([115,255,255])
+    mask = cv.inRange(hsv, lower, upper)
+    cv.imshow('Mask', mask)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    pos_avgs = get_band_averages(mask)
+    print(pos_avgs)
