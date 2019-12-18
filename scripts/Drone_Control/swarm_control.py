9dd050a2c007e760ece6332442e12348b1252273
RS-Coop <simpson.r.cooper@gmail.com>
2019-12-17 17:51:49 -0700

    Line following and move onto line ready to test

--- scripts/Drone_Control/swarm_control.py
+++ scripts/Drone_Control/swarm_control.py
@@ -6,13 +6,9 @@ from Intro_Robotics_Final_Project.msg import QR, EdgeList, DroneCommand
 #This class deals with controlling all drones
 class SwarmController:
     # Region definitions for image
-    # This is the center of the images being input to drone controller
     CENTER = G.BEBOP_CENTER
-    # Allowable error from center
     CENTER_X_ERROR = G.QR_ERROR
     CENTER_Y_ERROR = G.QR_ERROR
-    # acceptable angle error off 0
-    ANGLE_ERROR = G.DEFAULT_ANGLE_ERROR
 
     #Drones in swarm (NOTE: Right now just 1)
     drones = ['/bebop']
@@ -71,7 +67,7 @@ class SwarmController:
     def run_state(self):
         print("State:", self.current_state)
         if self.current_state == G.TAKEOFF:
-            self.takeoff() #Center qr code
+            self.launch_swarm() #Center qr code
             return False
         elif self.current_state == G.CENTER_QR:
             self.center_qr() #Center qr code
@@ -100,8 +96,8 @@ class SwarmController:
     def center_qr(self):
         centroid = self.qr_data["centroid"]
 
-        y_err = self.CENTER[0]-centroid[0] #pos means forward
-        x_err = self.CENTER[1]-centroid[1]  #pos means left
+        x_err = self.CENTER[0]-centroid[0] #pos means forward
+        y_err = self.CENTER[1]-centroid[1]  #pos means left
 
         print("XERR:", x_err, "YERR:", y_err)
         print("", abs(x_err), ">", self.CENTER_X_ERROR, "or", abs(y_err), ">", self.CENTER_Y_ERROR)
@@ -111,11 +107,11 @@ class SwarmController:
             #Determine the drone cmd
             cmd.drone_id = self.drones[0] #Note this would need to be changed for multiple drones
             #x movement
-            cmd.cmd_type.append("x")
+            cmd.cmd_type.append(G.X)
             cmd.intensity.append(0) #Will default to base intensity
             cmd.direction.append(np.sign(x_err))
             #y movement
-            cmd.cmd_type.append("y")
+            cmd.cmd_type.append(G.Y)
             cmd.intensity.append(0) #Will default to base intensity
             cmd.direction.append(np.sign(y_err))
 
@@ -150,7 +146,7 @@ class SwarmController:
     def move_onto_line(self):
         if self.qr_data["hasQR"] == True:
             cmd = DroneCommand()
-            
+
             #If the line is not vertical
             if self.is_line_vertical == False:
                 #Rotate to get the line vertical
@@ -158,7 +154,10 @@ class SwarmController:
 
                 cmd.cmd_type.append("angular")
                 cmd.intensity.append(0) #Will default to base intensity
-                cmd.direction.append() #Not sure about this agrument
+                #Do some stuff here to turn
+                x_err = self.CENTER[0] - centroid[0]
+                cmd.direction.append(np.sign(x_err)) #Not sure about this agrument
+
             #If the line is not centered
             elif self.is_line_centered == False:
                 #Shift left or right to center line
@@ -167,7 +166,7 @@ class SwarmController:
                 x_err = self.CENTER[1]-centroid[1] #pos means left
 
                 cmd.cmd_type.append("y")
-                cmd.intensity.append(0) #Will defualt to base intensity
+                cmd.intensity.append(0.2) #Will defualt to base intensity
                 cmd.direction.append(np.sign(x_err))
 
             #If the line is vertical and centered
@@ -182,12 +181,7 @@ class SwarmController:
             self.drone_command_pub.publish(cmd)
 
         else:
-            #Add the end vertex to the edge
-            current_edge = self.get_edge_pose(self.current_edge_color)
-            self.update_v2(self.current_edge_color, self.graph_edges, current_edge["v1"], self.qr_data["value"])
-            self.current_edge_color = None
-            #Change state
-            self.current_state = G.CENTER_QR
+            self.current_state = G.FOLLOW_LINE
 
     #Dispatches drone from a vertex to a edge
     #TODO: Select an edge leaving the vertex and go there
@@ -199,6 +193,7 @@ class SwarmController:
     #NOTE: Not yet fully implemented
     def follow_line(self):
         if self.qr_data["hasQR"] == False:
+
             cmd = DroneCommand()
             #If the line is not centered
             if self.is_line_centered == False:
@@ -207,7 +202,7 @@ class SwarmController:
                 #We should just care about x error
                 x_err = self.CENTER[1]-centroid[1] #pos means left
 
-                cmd.cmd_type.append("y")
+                cmd.cmd_type.append(G.Y)
                 cmd.intensity.append(0) #Will defualt to base intensity
                 cmd.direction.append(np.sign(x_err))
 
@@ -216,14 +211,14 @@ class SwarmController:
                 #Rotate to get the line vertical
                 centroid, angle = self.get_line_pose(self.current_edge_color)
 
-                cmd.cmd_type.append("angular")
-                cmd.intensity.append(0) #Will default to base intensity
-                cmd.direction.append() #Not sure about this agrument
+                cmd.cmd_type.append(G.THETA)
+                cmd.intensity.append(0.1) #Will default to base intensity
+                cmd.direction.append(np.sign(angle)) #Not sure about this agrument
 
             #If the line is vertical and centered
             else:
                 #Move forward
-                cmd.cmd_type.append("x")
+                cmd.cmd_type.append(G.X)
                 cmd.intensity.append(0) #Will default to base intensity
                 cmd.direction.append(1)
 
@@ -238,77 +233,73 @@ class SwarmController:
             self.current_state = G.CENTER_QR
 
     # Return true if the line is veritical in the image with a certain error
-    def is_line_vertical(self, line_color):
-        ((c_x, c_y), a) = get_line_pose(line_color)
+    def is_line_vertical(self):
+        centroid, angle = get_line_pose(line_color)
 
-        if abs(a) > self.ANGLE_ERROR:
+        if abs(angle) > self.ANGLE_ERROR:
             return True
+
         return False
 
     # Return true if the line is centered in the image with a certain error
-    def is_line_centered(self, line_color):
-        ((c_x, c_y), a) = get_line_pose(line_color)
+    def is_line_centered(self):
+        centroid, angle = get_line_pose(line_color)
 
-        x_err = self.CENTER[0]-c_x
-        y_err = centroid[1]-self.CENTER[1]
+        x_err = np.abs(self.CENTER[1]-centroid[1])
 
-        if abs(x_err) > self.CENTER_X_ERROR:
+        if x_err > self.CENTER_X_ERROR:
             return True
+
         return False
 
-    # Returns ((centroidx, y), angle)
+    # Returns centroid, angle
     def get_line_pose(self, line_color):
-        currentLine = get_edge_pose(line_color)
+        currentLine = self.get_edge_pose(line_color)
         zone_names_outer = [ "O_TOP", "O_BOTTOM", "O_LEFT" , "O_RIGHT"]
         zone_names_inner = ["I_TOP", "I_BOTTOM", "I_LEFT", "I_RIGHT"]
 
         if currentLine == None:
             return None, None
         else:
-            firstPoint = (None,None)
-            secondPoint = (None,None)
+            firstPoint = None
+            secondPoint = None
 
-            #Checking outer ring for two points
             for i in zone_names_outer:
-                if((currentLine["pos_avg"][i] is not (None,None)) and (firstPoint is (None,None))):
+                if((currentLine["pos_avg"][i] is not None) and (firstPoint is None)):
                     firstPoint = currentLine["pos_avg"][i]
-                elif((currentLine["pos_avg"][i] is not (None,None)) and (secondPoint is (None,None))):
+                elif((currentLine["pos_avg"][i] is not None) and (secondPoint is None)):
                     secondPoint = currentLine["pos_avg"][i]
                     break
 
-            #A second point was not found in the outer ring, checking inner ring
-            if(firstPoint is not (None,None) and secondPoint is (None,None)):
+
+            if(firstPoint is not None and secondPoint is None):
                 for i in zone_names_inner:
-                    if(currentLine["pos_avg"][i] is not (None,None)):
+                    if(currentLine["pos_avg"][i] is not None):
                         secondPoint = currentLine["pos_avg"][i]
                         break
-            
-            #If either point is not found, not enough data to calculate centroid and angle, return None
-            if(firstPoint is (None,None) or secondPoint is (None,None)):
-                return (None, None)
+
+
+            if(firstPoint is None or secondPoint is None):
+                return None, None
             else:
                 centroid = ((firstPoint[0] + secondPoint[0]) / 2 , (firstPoint[1] + secondPoint[1]) / 2)
                 x = np.abs(firstPoint[0] - secondPoint[0])
                 y = np.abs(firstPoint[1] - secondPoint[1])
-                theta = np.arcsin( x / np.sqrt(x**2 + y**2))
-                return (centroid, theta)
+                theta = np.arccos( y / np.sqrt(x**2 + y**2))
+                return centroid, theta
 
         return None, None
-
-
-    # TODO
-    # Has the drones take off!
-    def takeoff(self):
-        # Make the drones take off here: TODO
-        # Change state
-        self.current_state = G.CENTER_QR
 
     #Launches all drones in swarm
     #DONE: Launch all drones in drones list
     def launch_swarm(self):
+        self.current_state = G.CENTER_QR
+
         for drone in self.drones:
-            drone.takeoff()
-            drone.move_camera()
+            cmd = DroneCommand()
+            cmd.drone_id = drone
+            cmd.cmd_type.append(G.TAKEOFF)
+            self.drone_command_pub.publish(cmd)
 
         sleep(2.0)
 
@@ -316,12 +307,18 @@ class SwarmController:
     #DONE: Land all drones in drones list
     def land_swarm(self):
         for drone in self.drones:
-            drone.land()
+            cmd = DroneCommand()
+            cmd.drone_id = drone
+            cmd.cmd_type.append(G.LAND)
+            self.drone_command_pub.publish(cmd)
 
     #Failsafe
     def failsafe(self):
         for drone in self.drones:
-            drone.failsafe()
+            cmd = DroneCommand()
+            cmd.drone_id = drone
+            cmd.cmd_type.append(G.EMERGENCY)
+            self.drone_command_pub.publish(cmd)
 
     # This helper funciton will add all of the edge colors in the edges array to the graph
     def add_edges_to_graph(self, edges):
